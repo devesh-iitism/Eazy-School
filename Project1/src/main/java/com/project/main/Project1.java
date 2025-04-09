6511285b02b19069b8a0e6fa61217c681c9dc807
Devesh Chaturvedi <dcdev001@gmail.com>
2025-04-09 15:53:59 +0530

    understand @Primary Annotation

--- Project1/src/main/java/com/project/main/Project1.java
+++ Project1/src/main/java/com/project/main/Project1.java
@@ -11,7 +11,7 @@ public class Project1 {
         System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle veh = context.getBean(Vehicle.class);
+        Vehicle veh = context.getBean("vehicle1", Vehicle.class);
         System.out.println("Vehicle name from Spring Context is: " + veh.getName());
     }
 }
