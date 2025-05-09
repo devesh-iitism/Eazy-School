f2123bbac0bc8969c7a99a65198383e1c4b70524
Devesh Chaturvedi <dcdev001@gmail.com>
2025-05-10 01:55:48 +0530

    csrf fix

--- eazyschool/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ eazyschool/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -15,7 +15,7 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf((csrf) -> csrf.disable())
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
                         .requestMatchers("/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
@@ -23,14 +23,14 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
                         .requestMatchers("/login").permitAll()
-                        .requestMatchers("/assets/**").permitAll())
+                        .requestMatchers("/logout").permitAll())
                 .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
                         .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
                 .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
                         .invalidateHttpSession(true).permitAll())
                 .httpBasic(Customizer.withDefaults());
-
         return http.build();
     }
 
